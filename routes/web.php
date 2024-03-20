475dc392c2bdfc4ac97e1ecd1c73bd58f055e42c
eieiphyu123 <miss.eiphyu@gmail.com>
2024-03-20 17:36:04 +0630

    update MYBLOGLARAVEL 20-3-24

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [App\Http\Controllers\FrontendController::class, 'index'])->name('front.index');
+Route::get('post/{postid}', [App\Http\Controllers\FrontendController::class, 'show'])->name('front.show');
+
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
